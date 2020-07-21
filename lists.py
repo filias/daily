5fdd642b2376df00af8bf015c4e26eb5a7ba02a1
Filipa Andrade <filipa.andrade@gmail.com>
2020-07-21 20:49:00 +0200

    Add h-index problem and solution

--- lists.py
+++ lists.py
@@ -1,3 +1,4 @@
+from typing import List
 """
 Given a list of numbers and a number k, return whether any two numbers from the list
 add up to k.
@@ -6,7 +7,7 @@ Bonus: Can you do this in one pass?
 """
 
 
-def prob01(lst, k):
+def add_to_k(lst: List, k: int) -> bool:
     i = 0
     j = 1
     lst = sorted(lst)
@@ -23,13 +24,13 @@ def prob01(lst, k):
     return False
 
 
-lst = [10, 15, 3, 7, 5, 12, 2, 16]
+numbers = [10, 15, 3, 7, 5, 12, 2, 16]
 k = 6
-assert not prob01(lst, k)
+assert not add_to_k(numbers, k)
 
-lst = [10, 15, 3, 7]
+numbers = [10, 15, 3, 7]
 k = 17
-assert prob01(lst, k)
+assert add_to_k(numbers, k)
 
 
 """
@@ -45,7 +46,7 @@ Follow-up: what if you can't use division?
 from functools import reduce
 
 
-def prob02(lst):
+def product_list(lst: List) -> List:
     output = []
 
     for idx, val in enumerate(lst):
@@ -58,12 +59,12 @@ def prob02(lst):
 
 # Tests
 lst = [3, 2, 1]
-prob02(lst)
+product_list(lst)
 
 lst = [1, 2, 3, 4, 5]
-assert prob02(lst) == [120, 60, 40, 30, 24]
+assert product_list(lst) == [120, 60, 40, 30, 24]
 lst = [3, 2, 1]
-assert prob02(lst) == [2, 3, 6]
+assert product_list(lst) == [2, 3, 6]
 
 
 """
@@ -77,7 +78,7 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 You can modify the input array in-place."""
 
 
-def prob04(lst):
+def missing_int(lst: List) -> int:
     new_list = sorted(lst)
     try:
         idx = new_list.index(1)
@@ -91,9 +92,9 @@ def prob04(lst):
 
 
 lst = [3, 4, -1, 1]
-assert prob04(lst) == 2
+assert missing_int(lst) == 2
 lst = [1, 2, 0]
-assert prob04(lst) == 3
+assert missing_int(lst) == 3
 
 
 """
@@ -107,7 +108,7 @@ Do this in O(N) time and O(1) space.
 from collections import defaultdict
 
 
-def prob40(lst):
+def non_duplicated_int(lst: List) -> int:
     result = defaultdict(int)
     for val in lst:
         result[val] += 1
@@ -118,8 +119,8 @@ def prob40(lst):
     return list(result.keys())[0]
 
 
-assert prob40([6, 1, 3, 3, 3, 6, 6]) == 1
-assert prob40([13, 19, 13, 13]) == 19
+assert non_duplicated_int([6, 1, 3, 3, 3, 6, 6]) == 1
+assert non_duplicated_int([13, 19, 13, 13]) == 19
 
 
 """
@@ -145,3 +146,36 @@ def balance_index(lst):
 
 assert balance_index([1, 2, 3, 6]) == 3
 assert balance_index([5, 2, 3, 1, 1, 1, 1]) == 2
+
+
+"""
+In academia, the h-index is a metric used to calculate the impact of a researcher's papers. It is calculated as follows:
+A researcher has index h if at least h of her N papers have h citations each. If there are multiple h satisfying this 
+formula, the maximum is chosen.
+
+For example, suppose N = 5, and the respective citations of each paper are [4, 3, 0, 1, 5]. Then the h-index would be 3, 
+since the researcher has 3 papers with at least 3 citations.
+
+Given a list of paper citations of a researcher, calculate their h-index.
+"""
+
+
+def h_index(lst: List) -> int:
+    h, count = 0, 0
+    lst = sorted(lst)
+    for item in reversed(lst):
+        if count == h and count != 0:
+            return h
+        else:
+            h = item
+            count += 1
+
+
+citations = [4, 3, 0, 1, 5]
+assert h_index(citations) == 3
+
+citations = [4, 3, 0, 1, 4, 6, 5]
+assert h_index(citations) == 4
+
+citations = [1, 0]
+assert h_index(citations) == 1
